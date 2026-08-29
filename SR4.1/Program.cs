ddfd50b63893c43de4ec075404f650edf5d8442f
Samarin <Samarin@DESKTOP-6O2D0HE>
2026-08-29 15:04:23 +0300

    Добавьте файлы проекта.

---
+++ SR4.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N>0");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            int n = 1;
+            for (int i = 1; i <= N; i++)
+            {
+                s+=n;
+                Console.WriteLine(s);
+                n+=2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
